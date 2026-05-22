6e3c9de59d69ee09a15c438f6e0a6cc444cd0f70
Luiz Miguel <contato.luiz404@gmail.com>
2026-05-22 18:24:15 -0300

    feat(ui): implement fully interactive boot selection menu with button debounce

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include "video.h"
 
-#define REG_VCOUNT *(volatile unsigned short*)0x04000006
+#define REG_KEYINPUT  *(volatile unsigned short*)0x04000130
+#define REG_VCOUNT    *(volatile unsigned short*)0x04000006
+
+#define KEY_UP     (1 << 6)
+#define KEY_DOWN   (1 << 7)
+#define KEY_A      (1 << 0)
 
 void wait_vblank(void) {
     while (REG_VCOUNT >= 192);
@@ -11,14 +16,48 @@ int main(void) {
     video_init();
     wait_vblank();
     
-    video_clear_screens();
-    video_draw_divider();
-
-    video_print_text("Luish Kernel", 8, 6);
-    video_print_text("Hello, made by Luiz Miguel", 8, 30);
+    int selected_index = 0; 
+    int total_options = 2;
+    
+    unsigned short last_keys = 0xFFFF;
 
     while (1) {
         wait_vblank();
+
+        video_clear_screens();
+        video_draw_divider();
+
+        video_print_text("Initialize the system?", 4, 4);
+
+        if (selected_index == 0) {
+            video_print_text("> Yes", 6, 8);
+            video_print_text("  No", 6, 10);
+        } else {
+            video_print_text("  Yes", 6, 8);
+            video_print_text("> No", 6, 10);
+        }
+
+        unsigned short current_keys = REG_KEYINPUT;
+        
+        unsigned short pressed = (last_keys ^ current_keys) & (~current_keys);
+        last_keys = current_keys;
+
+        if (pressed & KEY_DOWN) {
+            selected_index = (selected_index + 1) % total_options;
+        } 
+        else if (pressed & KEY_UP) {
+            selected_index = (selected_index - 1 + total_options) % total_options;
+        }
+        
+        if (pressed & KEY_A) {
+            video_clear_screens();
+            if (selected_index == 0) {
+                video_print_text("Booting Luish Kernel...", 4, 8);
+            } else {
+                video_print_text("Boot Aborted.", 4, 8);
+            }
+            while(1) { wait_vblank(); }
+        }
     }
 
     return 0;
