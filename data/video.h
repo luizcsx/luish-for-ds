55e7ab26a88344875e7fa26b045aa023d35e103e
Luiz Miguel <contato.luiz404@gmail.com>
2026-05-22 22:33:57 -0300

    refactor(video): expand video header to support target parameters

--- data/video.h
+++ data/video.h
@@ -1,9 +1,9 @@
 #ifndef VIDEO_H
 #define VIDEO_H
 
-#define COLOR_BACKGROUND  0x0000
-#define COLOR_TEXT        0xFFFF
-#define COLOR_ACCENT      0x03E0
+#define COLOR_BACKGROUND 0x0000 
+#define COLOR_TEXT       0x7FFF 
+#define COLOR_ACCENT     0x03E0 
 
 void video_init(void);
 void video_clear_screens(void);
