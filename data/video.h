d00321968a6da9605532b0f609ed72271db0e952
Luiz Miguel <contato.luiz404@gmail.com>
2026-05-22 21:09:27 -0300

    feat(video): support top screen initialization

--- data/video.h
+++ data/video.h
@@ -1,13 +1,13 @@
-#ifndef LUISH_DATA_VIDEO_H
-#define LUISH_DATA_VIDEO_H
+#ifndef VIDEO_H
+#define VIDEO_H
 
 #define COLOR_BACKGROUND  0x0000
-#define COLOR_TEXT        0x7FFF
-#define COLOR_ACCENT      0x39E7
+#define COLOR_TEXT        0xFFFF
+#define COLOR_ACCENT      0x03E0
 
 void video_init(void);
 void video_clear_screens(void);
 void video_draw_divider(void);
 void video_print_text(const char* text, int x, int y);
 
-#endif // LUISH_DATA_VIDEO_H
+#endif
