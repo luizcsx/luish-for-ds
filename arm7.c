0d002d313a80a9a12e7cb7f26bbbeb4fedc332ba
Luiz Miguel <contato.luiz404@gmail.com>
2026-05-23 17:31:08 -0300

    fix: command arm7 to wake screen power management system

--- arm7.c
+++ arm7.c
@@ -1,5 +1,10 @@
+#define REG_POWERCNT_7  *(volatile unsigned short*)0x04000304
+
 int main(void) {
+    REG_POWERCNT_7 |= 0x0001; 
+
     while (1) {
+        __asm__("swi 0x060000");
     }
     return 0;
 }
