1609f0b4be6f9e7e77fd8d8fcb41aab55e470050
Luiz Miguel <contato.luiz404@gmail.com>
2026-05-22 22:51:50 -0300

    chore: reset video header to bypass legacy declarations

--- data/video.h
+++ data/video.h
@@ -1,14 +1,4 @@
 #ifndef VIDEO_H
 #define VIDEO_H
 
-#define COLOR_BACKGROUND 0x0000 
-#define COLOR_TEXT       0x7FFF 
-#define COLOR_ACCENT     0x03E0 
-
-void video_init(void);
-void video_clear_screens(void);
-void video_draw_divider(void);
-void video_print_text(const char* text, int x, int y);
-void video_print_text_ext(const char* text, int x, int y, int target_screen);
-
 #endif
